7467f2ba1bb4e428393e4e8853e18f2694851a46
snowphone <mjo970625@gmail.com>
2018-12-15 20:24:50 +0900

    Complete touch implementation

--- sfs_func_hw.c
+++ sfs_func_hw.c
@@ -7,6 +7,8 @@
 #include <stdlib.h>
 #include <string.h>
 #include <assert.h>
+#include <stdint.h> //to use uint32_t
+#include <stdbool.h>
 
 /* optional */
 #include <sys/types.h>
@@ -52,6 +54,9 @@ enum ERROR{
 	CD_NOT_DIR = -2,
 	CD_NOT_EXISTS = -1,
 	LS_NOT_EXISTS = -1,
+	TOUCH_BLOCK_UNAVAILABLE = -4,
+	TOUCH_DIRECTORY_FULL = -3,
+	TOUCH_ALREADY_EXISTS = -6,
 };
 
 void error_message(const char *message, const char *path, int error_code) {
@@ -112,7 +117,6 @@ void sfs_mount(const char* path)
 }
 
 void sfs_umount() {
-
 	if( sd_cwd.sfd_ino !=  SFS_NOINO )
 	{
 		//umount
@@ -123,56 +127,79 @@ void sfs_umount() {
 	}
 }
 
-void sfs_touch(const char* path)
+_Bool findUnusedBlock(uint32_t* blockPtr, uint32_t* bytePtr, uint32_t* bitPtr)
 {
-	//skeleton implementation
+	const int end = SFS_MAP_LOCATION + SFS_BITBLOCKS(spb.sp_nblocks);
+	int i,
+		block_num;
+	for(block_num = SFS_MAP_LOCATION; block_num < end; ++block_num)
+	{
+		uint8_t buf[SFS_BLOCKSIZE] = { 0, };
+		disk_read(buf, block_num);
 
-	struct sfs_inode si;
-	disk_read( &si, sd_cwd.sfd_ino );
-
-	//for consistency
-	assert( si.sfi_type == SFS_TYPE_DIR );
-
-	//we assume that cwd is the root directory and root directory is empty which has . and .. only
-	//unused DISK2.img satisfy these assumption
-	//for new directory entry(for new file), we use cwd.sfi_direct[0] and offset 2
-	//becasue cwd.sfi_directory[0] is already allocated, by .(offset 0) and ..(offset 1)
-	//for new inode, we use block 6 
-	// block 0: superblock,	block 1:root, 	block 2:bitmap 
-	// block 3:bitmap,  	block 4:bitmap 	block 5:root.sfi_direct[0] 	block 6:unused
-	//
-	//if used DISK2.img is used, result is not defined
-	
-	//buffer for disk read
-	struct sfs_dir sd[SFS_DENTRYPERBLOCK];
-
-	//block access
-	disk_read( sd, si.sfi_direct[0] );
-
-	//allocate new block
-	int newbie_ino = 6;
-
-	sd[2].sfd_ino = newbie_ino;
-	strncpy( sd[2].sfd_name, path, SFS_NAMELEN );
-
-	disk_write( sd, si.sfi_direct[0] );
-
-	si.sfi_size += sizeof(struct sfs_dir);
-	disk_write( &si, sd_cwd.sfd_ino );
-
-	struct sfs_inode newbie;
-
-	bzero(&newbie,SFS_BLOCKSIZE); // initalize sfi_direct[] and sfi_indirect
-	newbie.sfi_size = 0;
-	newbie.sfi_type = SFS_TYPE_FILE;
-
-	disk_write( &newbie, newbie_ino );
+		for(i = 0; i != SFS_BLOCKSIZE; ++i)
+		{
+			//Read block as a byte stream for speed
+			if(buf[i] == ( 1u << CHAR_BIT ) - 1 )
+				continue;
+			int j;
+			for(j = 0; j < CHAR_BIT; ++j)
+			{
+				if( BIT_CHECK(buf[i], j) == 0 ) {
+					*blockPtr = block_num;
+					*bytePtr = i;
+					*bitPtr = j;
+					return true;
+				}
+			}
+		}
+	}
+	return false;
 }
 
-_Bool isDirectory(const u_int32_t inode_num) {
-	struct sfs_inode inode;
-	disk_read(&inode, inode_num);
-	return inode.sfi_type == SFS_TYPE_DIR;
+void setBlock(const int blockIdx, const int bytesIdx, const int bitsIdx)
+{
+	u_int8_t buf[SFS_BLOCKSIZE] = { 0, };
+	disk_read(buf, blockIdx);
+	
+	BIT_SET(buf[bytesIdx], bitsIdx);
+
+	disk_write(buf, blockIdx);
+}
+
+void releaseBlock(uint32_t inodeNum)
+{
+	uint32_t block_num = SFS_MAP_LOCATION + inodeNum / SFS_BLOCKBITS,
+			  bytesIdx = (inodeNum >> CHAR_BIT) & (SFS_BLOCKSIZE - 1),
+			  bitsIdx = inodeNum % CHAR_BIT;
+
+	u_int8_t buf[SFS_BLOCKSIZE];
+	disk_read(buf, block_num);
+	
+	BIT_CLEAR(buf[bytesIdx], bitsIdx);
+
+	disk_write(buf, block_num);
+}
+
+int64_t getUnusedBlock()
+{
+	uint32_t blockIdx, bytesIdx, bitsIdx;
+	if(!findUnusedBlock(&blockIdx, &bytesIdx, &bitsIdx))
+		return -1;
+	setBlock(blockIdx, bytesIdx, bitsIdx);
+
+	return (blockIdx - SFS_MAP_LOCATION) * SFS_BLOCKBITS 
+		+ bytesIdx * CHAR_BIT 
+		+ bitsIdx;
+}
+
+uint32_t getNumOfFilesInDirectory(const struct sfs_dir* dirPtr)
+{
+	struct sfs_inode dirInode;
+	disk_read(&dirInode, dirPtr->sfd_ino);
+	assert(dirInode.sfi_type == SFS_TYPE_DIR);
+
+	return dirInode.sfi_size / sizeof(struct sfs_dir);
 }
 
 _Bool isSameString(const char* lhs, const char* rhs){
@@ -188,15 +215,105 @@ void* findStrInDirEntries(struct sfs_dir* entry, const char* str){
 	return NULL;
 }
 
-u_int32_t getBlocksToInvestigate(const u_int32_t bytes){
+void sfs_touch(const char* path)
+{
+	uint32_t numOfFiles = getNumOfFilesInDirectory(&sd_cwd);
+	uint32_t i;
+	// cwd inode
+	struct sfs_inode inode;
+	disk_read( &inode, sd_cwd.sfd_ino );
+
+	//for consistency
+	assert( inode.sfi_type == SFS_TYPE_DIR );
+
+	//we assume that cwd is the root directory and root directory is empty which has . and .. only
+	//unused DISK2.img satisfy these assumption
+	//for new directory entry(for new file), we use cwd.sfi_direct[0] and offset 2
+	//becasue cwd.sfi_directory[0] is already allocated, by .(offset 0) and ..(offset 1)
+	//for new inode, we use block 6 
+	// block 0: superblock,	block 1:root, 	block 2:bitmap 
+	// block 3:bitmap,  	block 4:bitmap 	block 5:root.sfi_direct[0] 	block 6:unused
+	//
+	
+	//check for empty entry in directoy inode's direct block pointer
+	if(inode.sfi_size / sizeof(struct sfs_dir) == MAX_NUM_OF_DIR ) {
+		error_message("touch", path, TOUCH_DIRECTORY_FULL);
+		goto exit;
+	}
+
+	//Check for filename duplication!
+	for(i = 0; i < numOfFiles / SFS_DENTRYPERBLOCK + 1; ++i)
+	{
+		struct sfs_dir entry_buf[SFS_DENTRYPERBLOCK];
+		disk_read(entry_buf, inode.sfi_direct[i]);
+
+		if(findStrInDirEntries(entry_buf, path)) {
+			error_message("touch", path, TOUCH_ALREADY_EXISTS);
+			goto exit;
+		}
+	}
+
+	//allocate new block
+	int newbie_ino = getUnusedBlock();
+	if(newbie_ino == -1) {
+		error_message("touch", path, TOUCH_BLOCK_UNAVAILABLE);
+		goto exit;
+	}
+
+	/* --------- No more exceptions! ----------- */
+
+	//buffer for disk read
+	struct sfs_dir entries[SFS_DENTRYPERBLOCK];
+
+	/* A direct block pointer can hold SFS_DENTRYPERBLOCK entries 
+	 * There's SFS_NDIRECT direct pointers
+	 * Therefore, when the number of files n is given then
+	 * access (n / SFS_DENTRYPERBLOCK)th pointer and 
+	 * access (n % SFS_DENTRYPERBLOCK)th entry!
+	 */
+
+	//block access
+	disk_read( entries, inode.sfi_direct[numOfFiles / SFS_DENTRYPERBLOCK] );
+
+	//find an empty direct pointer
+	struct sfs_dir* newbie_entry = entries + numOfFiles % SFS_DENTRYPERBLOCK;
+	newbie_entry->sfd_ino = newbie_ino;
+	strncpy( newbie_entry->sfd_name, path, SFS_NAMELEN );
+
+	disk_write( entries, inode.sfi_direct[numOfFiles / SFS_DENTRYPERBLOCK] );
+
+	inode.sfi_size += sizeof(struct sfs_dir);
+	disk_write( &inode, sd_cwd.sfd_ino );
+
+	struct sfs_inode newbie;
+
+	bzero(&newbie,SFS_BLOCKSIZE); // initalize sfi_direct[] and sfi_indirect
+	newbie.sfi_size = 0;
+	newbie.sfi_type = SFS_TYPE_FILE;
+
+	disk_write( &newbie, newbie_ino );
+exit:
+	return;
+}
+
+_Bool isDirectory(const uint32_t inode_num) {
+	struct sfs_inode inode;
+	disk_read(&inode, inode_num);
+	return inode.sfi_type == SFS_TYPE_DIR;
+}
+
+
+
+uint32_t getBlocksToInvestigate(const u_int32_t bytes){
 	return bytes / SFS_BLOCKSIZE + 1;
 }
 
-void* findInCwd(const char* path){
+void* findInCwd(const char* path)
+{
 	struct sfs_inode inode;
 	disk_read(&inode, sd_cwd.sfd_ino);
 
-	u_int32_t i;
+	uint32_t i;
 	for(i = 0; i < getBlocksToInvestigate(inode.sfi_size); ++i)
 	{
 		struct sfs_dir entries[SFS_DENTRYPERBLOCK] = {0, };
@@ -214,7 +331,7 @@ void sfs_cd(const char* path)
 {
 	if(!path) {
 		sd_cwd.sfd_ino = 1;
-		strcpy(sd_cwd.sfd_name, "/");
+		strncpy(sd_cwd.sfd_name, "/", SFS_NAMELEN );
 		goto exit;
 	}
 
@@ -260,15 +377,15 @@ void sfs_ls(const char* path)
 		struct sfs_inode inode;
 		disk_read(&inode, entry->sfd_ino);
 
-		u_int32_t i, 
-				  totalFiles = inode.sfi_size / sizeof(struct sfs_dir),
-				  cnt = 0;
+		uint32_t i, 
+				 totalFiles = inode.sfi_size / sizeof(struct sfs_dir),
+				 cnt = 0;
 		for(i = 0; i < getBlocksToInvestigate(inode.sfi_size); ++i)
 		{
 			struct sfs_dir entries[SFS_DENTRYPERBLOCK];
 			disk_read(entries, inode.sfi_direct[i]);
 
-			u_int32_t j;
+			uint32_t j;
 			for(j = 0; j < SFS_DENTRYPERBLOCK; ++j)
 			{
 				++cnt;
@@ -279,7 +396,6 @@ void sfs_ls(const char* path)
 				if(cnt == totalFiles)
 					goto exit;
 			}
-
 		}
 	} else {
 		/* list specific file */
