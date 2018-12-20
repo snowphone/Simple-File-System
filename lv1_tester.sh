ffd0986d9bd4fe9ff64174f0a29a85780d4d0f41
snowphone <mjo970625@gmail.com>
2018-12-20 11:24:06 +0900

    Modify display format like lv2

--- lv1_tester.sh
+++ lv1_tester.sh
@@ -7,5 +7,18 @@ expected=$(./sfs < test_lscd)
 git checkout DISK1.img
 actual=$(./mysfs < test_lscd)
 
+git checkout DISK1.img
+
+actual=$(./mysfs < test_lscd)
 diff <(echo "$expected") <(echo "$actual") 
+
+if [ $? -eq '0' ]
+then
+	echo "lscd test: Success"
+else
+	echo "----------expected-----------"
+	echo "$expected"
+	echo "-----------actual------------"
+	echo "$actual"
+fi
 
